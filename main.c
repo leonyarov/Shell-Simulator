8e0892dcbb0e71acc442d3a24432e45ab964aaae
leonyarov <leon44ml@gmail.com>
2021-08-15 22:17:52 +0300

    parse, readline

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+int main(int argc, char **argv){
+    int childPid;
+    char * cmdLine;
+    parseInfo *info;
+    while (1){
+
+        cmdLine = readline(">");
+        info = parse(cmdLine);
+        childPid = fork();
+
+        if (childPid == 0){
+
+            executeCommand(info); // calll execvp
+        }
+        else{
+            waitpid(childPid);
+        }
+    }
+    free(cmdLine);
+    return 0;
+}
---
+++ shell.h
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct parseInfo
+{
+    char *cmd;
+    char **args;
+} parseInfo;
+
+// Read line from the console, print the symbol before
+char *readline(char *symbol)
+{
+    size_t bufsize = 1024;
+    char *line = NULL;
+    printf("%s ", symbol);
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+
+parseInfo* parse(char *line)
+{
+    char *token;       // temporary token pointer
+    parseInfo cmds[2]; // amout of commands required to store (2)
+    char *pipes[2];    // command pipes temp pointer
+
+    char *pipe = strtok(line, "|"); // get the first command
+
+    // Seperate the line by pipes '|'
+    for (int i = 0; pipe != NULL || i < 2; i++)
+    {
+        pipes[i] = pipe;
+        pipe = strtok(NULL, "|");
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        // get the command - cat, touch, wc etc...
+        token = strtok(pipes[i], " ");
+        cmds[i].cmd = token;
+
+        // go through command arguments - -t, -s, >, directories
+        for (int j = 0; token != NULL; j++)
+        {
+            token = strtok(NULL, " ");
+
+            // allocate new space for an arg token
+            cmds[i].args = (char **)realloc(cmds[i].args, (j + 1) * sizeof(char *));
+
+            // assign token based on type
+
+            // token is char arg
+            if (token[0] == '-')
+                cmds[i].args[j] = &token[1];
+            // token is path or special symbol > or <
+            else
+                cmds[i].args[j] = token;
+        }
+    } 
+
+    return cmds;
+}
+
+void executeCommand(parseInfo *info)
+{
+    for (size_t i = 0; i < 2; i++)
+    {
+        char* cmd = info[i].cmd;
+        
+    }
+    
+}
