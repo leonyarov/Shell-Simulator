85e3683a2060dfa665b14b451a4b819bb09e3cb4
Daniel Grounin <69406706+Daniel-Grounin@users.noreply.github.com>
2023-04-18 00:42:15 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,21 +2,26 @@
 //Daniel Grounin 319191680
 
 
+
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
-#include <readline/readline.h> //provide function readline() that reads a line of text from user and returns it as a dynamically aclocated string.
-#include <readline/history.h> // manage the command history line
+#include <readline/readline.h>
+#include <readline/history.h>
 
-typedef struct parseInfo
-{
-    char *cmd;
-    char **args;
-} parseInfo;
 
-// Read line from the console, print the symbol before
-char *readline(char *symbol);
 
-parseInfo* parse(char *line);
+typedef struct parseInfo {
+    char ** command;
+    int size;
+    int pipe_index;
+}parseInfo;
 
-void executeCommand(parseInfo *info);
+void freeInfo(parseInfo* info);
+void printPath();
+void execute_cat(parseInfo* info);
+parseInfo* parse(char* );
+void executeCommand(parseInfo * info);
+void execute_pipe(int,char*);
