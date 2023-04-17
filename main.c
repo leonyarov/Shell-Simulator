e0533439b0f9087859bede8da0c92944c406ecf0
Daniel Grounin <69406706+Daniel-Grounin@users.noreply.github.com>
2023-04-18 00:41:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,41 @@
 //Leon Yarovinski *********
 //Daniel Grounin 319191680
 
-#include "shell.c"
 
-int main(int argc, char **argv){
+#include "funcs.c"
+
+int main(int argc, char** argv) {
     int childPid;
-    char * cmdLine;
-    parseInfo *info;
-    while (1){
+    char* cmdLine = NULL;
+    parseInfo* info = NULL;
 
-        cmdLine = readline(">");
+    printf(BGRN"\n---------------STARTING CUSTOM SHELL---------------\n");
+    while (1) {
+        printPath();
+        cmdLine = readline(BGRN" $ ");
+        add_history(cmdLine);
         info = parse(cmdLine);
+
+        if (info->command && strcmp(info->command[0], "exit") == 0) {
+            freeInfo(info);
+            exit(1);
+        }
+        else if (info->command && strcmp(info->command[0], "cd") == 0) {
+            if (chdir(info->command[1]) == -1) {
+                printf("%s NOT FOUND\n", info->command[1]);
+                freeInfo(info);
+            }
+            continue;
+        }
+
         childPid = fork();
 
-        if (childPid == 0){
-
-            executeCommand(info); // calll execvp
+        if (childPid == 0) {
+            executeCommand(info);
+            freeInfo(info);
         }
-        else{
+        else {
             waitpid(childPid);
         }
     }
-    free(cmdLine);
-    return 0;
 }
